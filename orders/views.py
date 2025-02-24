0e312ca85cc7af1d449343c2a4891d2a72d2a7fb
raais8 <aleixribastorras@gmail.com>
2025-02-24 20:06:34 +0100

    fix: Fixed marketplace filter and added mor search fields.

--- orders/views.py
+++ orders/views.py
@@ -8,8 +8,19 @@ from .serializers import OrderListSerializer
 from .pagination import OrderPagination
 
 class OrderListView(ListAPIView):
-    search_fields = ["order_id", "ticket"]
+    search_fields = ["order_id", "ticket", "customer__bill_firstname", "customer__bill_lastname", "customer__ship_firstname", "customer__ship_lastname"]
     filter_backends = (filters.SearchFilter,)
     queryset = Order.objects.order_by("-order_date")
     serializer_class = OrderListSerializer
     pagination_class = OrderPagination
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        marketplaces = self.request.query_params.get("marketplace")
+        if marketplaces:
+            marketplaces_list = marketplaces.split(",")
+            queryset = queryset.filter(marketplace_id__in=marketplaces_list)
+
+
+        return queryset
